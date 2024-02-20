263f7115dfe0ad9f7bdc6af3ef2ca77ee4d57b1b
ElFalsoLicendiado <chopytrollface@gmail.com>
2024-02-19 20:06:22 -0600

    Ejercicio 1 Persona

--- Main.java
+++ Main.java
@@ -2,5 +2,10 @@ public class Main {
 
     public static void main (String[] args) {
         
+        Persona person1 = new Persona("Juan", 16, "hombre");
+        Persona person2 = new Persona("Sofia", 30, "mujer");
+        Persona person3 = new Persona("Jorge", 67, "hombre");
+        Persona person4 = new Persona("Julieta", 29, "mujer");
+
     }
 }
--- Persona.java
+++ Persona.java
@@ -1,3 +1,20 @@
 public class Persona {
     
+    String name= "";
+    int age ;
+    String gender;
+
+
+    public Persona(String name, int age, String gender){
+        this.name=name;
+        this.age=age;
+        this.gender=gender;
+        Print();
+    }
+    
+    public void Print (){
+        System.out.println("Su nombre es: "+name+", tiene "+age+ " años de edad y es: "+gender);
+    }
+
 }
+
