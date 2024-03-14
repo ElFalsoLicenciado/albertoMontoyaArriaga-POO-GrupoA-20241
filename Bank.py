5902b0a2a4e0b6d312c4c5f490bc1ecc6bf76275
ElFalsoLicendiado <chopytrollface@gmail.com>
2024-03-13 20:50:35 -0600

    Paso a python v1.1

--- Bank.py
+++ Bank.py
@@ -2,5 +2,120 @@ import Employee
 import BankAccount
 
 class Bank:
-    pass
+  __ui = ""
+  __flag = False
+  
+
+  def __init__(self):
+    __bank_accounts = []
+    __emp_list = []
+
+
+  def create_account (self):
+   type = '~'
+
+   while True:
+      print("What type of account do you want?")
+      print("A type: MAX $50 000\nB type: MAX $100 000\nC type: unlimited\n")
+
+      while True:
+        try :
+          self.__ui = input()
+          self.__ui = self.__ui.lower()
+          type = self.__ui
+          break
+        except TypeError:
+          print("Invalid input.")
+      
+      if (Bank.confirmation()==True):
+        print("Write the amount")
+        
+        break
+
+  
+  def create_employee (self):
+    name = input("Write the name.")
+    last_name = input("Write the last name")
+
+
+  def confirmation (self):
+    ui = ""
+    ui = input("Are you sure? ")
+    f = False
+    
+    if(ui.lower("y") or ui.lower("yes")):
+      f = True
+    else:
+      pass
+
+    return f
+
+
+class Main:
+
+  
+
+  def menu ():
+   banco = Bank()
+   while True:
+    
+    print("1. Employees and bank accounts.")
+    print("2. Search for all bank accounts.")
+    print("3. Search for a specific account.")
+    print("0. Quit")
+    opt = input()
+    
+    if opt == 1:
+      
+      while True:
+
+        print("1. Register employee.")
+        print("2. Regiser employee + account.")
+        print("3. Regiser employee + account & founds.")
+        print("0. Return.")
+        p = input()
+
+        if p == 1:
+          banco.create_employee()
+          break
+        
+        elif p == 2:
+          name = input("Employee's name: ")
+          last_name = input("Employee's last name: ")
+          employee = Employee(name,last_name)
           
+          type = input().lower()
+
+          break
+        
+        elif p == 3:
+          
+          break
+
+        elif p == 0:
+          
+          break
+
+        else:
+          print("Invalid input.")
+
+
+        
+
+
+    elif opt == 2:
+      while True:
+        pass
+        
+    elif opt == 3:
+      while True:
+        pass
+
+
+    elif opt == 0:
+      print("Cheers")
+      break
+
+    else:
+      print("Invalid choice")
+    
--- BankAccount.py
+++ BankAccount.py
@@ -7,10 +7,16 @@ class BankAccount:
     __acc_type = ''
     __mx_funds = 0.0
     __mn_funds = 0.0
-    __valid_acc = True
 
-    def __init__(self):
+
+    def __init__(self, type, amount):
         self.__acc_number = random.randint(9999,10000)
+
+        if self.__acc_type(type) == True:
+            self.add_funds(amount)
+        else :
+            del self
+
 
     def set_account_type(self, acc_type):
         self.__acc_type = acc_type
@@ -27,32 +33,26 @@ class BankAccount:
             self.__mx_funds = sys.float_info.max
             self.__mn_funds = 10000
         
-        else:  
-            print("Could not create or modify that account type")
-            self.__valid_acc = False
-            self.__acc_type = '~' 
 
-    def set_valid_account (self, status):
-        self.__valid_acc = status
-    
     def get_account_number (self):
         return self.__acc_number
 
+
     def get_acc_type (self):
         return self.__acc_type
-    
+
+
     def get_funds (self):
         return self.__funds
-    
-    def get_validity(self):
-        return self.__valid_acc
-    
+
+
     def add_funds (self, amount):
         if(self.__funds + amount > self.__mx_funds or amount < 1):
             print("Couldn't deposit that amount.")
         else:
             self.__funds += amount
             print(f"You added: ${self.__funds}")
+
 
     def take_funds (self, amount):
         if(self.__funds - amount < self.__mn_funds or amount < 1):
